07cca362a564a64433d4cb8906c08dd31164b2bf
wangdi <diwang@wicresoft.com>
2013-06-23 17:27:09 +0800

    login

---
+++ Campus.Course.Business.Interface/ILogin.cs
@@ -0,0 +1,15 @@
+﻿using Campus.Course.Model;
+using Campus.Course.Model.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Campus.Course.Business.Interface
+{
+    public interface ILogin
+    {
+        bool CheckLogin(CampusEntities context, string user, string password, bool isStudent);
+        V_CurrentUser GetCurrentUser(CampusEntities context, string userno, bool isStudent);
+    }
+}
--- Campus.Course/BaseController.cs
+++ Campus.Course/BaseController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Campus.Course.Model.Business;
+using Campus.Course.Business.Interface;
+using Campus.Course.Model.Business;
+using Campus.Course.Business;
 
 namespace Campus.Course
 {
@@ -17,6 +21,23 @@ namespace Campus.Course
                 Data = raw.ToString(),
                 JsonRequestBehavior = behavior
             };
+        }
+
+        public V_CurrentUser CurrentUser
+        {
+            get
+            {
+                V_CurrentUser current = new Model.Business.V_CurrentUser();
+                string userName = HttpContext.User.Identity.Name;
+                Login login = new Login();
+                current = Session["V_CurrentUser"] as V_CurrentUser;
+                if (current == null)
+                {
+                    current = login.GetCurrentUser(null, userName, true);
+                    Session["V_CurrentUser"] = current;
+                }
+                return current;
+            }
         }
 
     }
