32cb508157c44349f81fefc9958d30b0b5a0433e
P_Chou <zhoupingtkbjb@163.com>
2013-06-26 11:12:30 +0800

    minicalendar for timesheet

---
+++ Campus.Course/Controllers/PreparationController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Campus.Course.Controllers
+{
+    public class PreparationController : BaseController
+    {
+        //
+        // GET: /Preparation/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
