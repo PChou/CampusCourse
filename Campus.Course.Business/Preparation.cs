1b2913b595d8800b3831df6481bc533e47d26056
P_Chou <zhoupingtkbjb@163.com>
2013-06-27 22:02:07 +0800

    temp commit in order to pull dev from wangdi

--- Campus.Course.Business/Preparation.cs
+++ Campus.Course.Business/Preparation.cs
@@ -1,5 +1,6 @@
 ﻿using Campus.Course.Business.Interface;
 using Campus.Course.Model;
+using Campus.Course.Model.Business;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,7 @@ namespace Campus.Course.Business
 {
     public class Preparation : IPreparation
     {
-        public void GetPreparationByPId(CampusEntities context,int PId)
+        public IEnumerable<PreparationInfo> GetPreparationByPId(CampusEntities context,int PId)
         {
             CampusEntities campus = null;
             if (context == null)
@@ -22,8 +23,64 @@ namespace Campus.Course.Business
             }
             try
             {
-               //var q = from prep in campus.Preparations
-               //        join 
+                var q = from prep in campus.Preparations
+                        join met in campus.PreparationMeteirals on prep.ID equals met.PreparationId
+                        into prepjmet
+                        from pt in prepjmet.DefaultIfEmpty()
+                        where prep.ID == PId
+                        select new PreparationInfo { 
+                            PrepId = prep.ID,
+                            PrepName = prep.PrepName,
+                            PrepContent = prep.PrepContent,
+                            MeteiralId = pt == null ? 0 : pt.ID,
+                            MeteiralName = pt == null ? null : pt.Name,
+                            MeteiralPath = pt == null ? null : pt.PhysicalStore,
+                            MeteiralType = pt == null ? null : pt.Type,
+                        };
+                return q.ToArray();
+                
+            }
+            finally
+            {
+                if (context == null)
+                    campus.Dispose();
+            }
+        }
+
+
+        public Model.Preparation SavePreparation(CampusEntities context, Model.Preparation preparation)
+        {
+            CampusEntities campus = null;
+            if (context == null)
+            {
+                campus = new CampusEntities();
+            }
+            else
+            {
+                campus = context;
+            }
+            try
+            {
+                if (preparation.ID == 0)//create
+                {
+                    campus.Preparations.Add(preparation);
+                    campus.SaveChanges();
+                    return preparation;
+                }
+                else
+                {
+                    var q = from prep in campus.Preparations
+                            where prep.ID == preparation.ID
+                            select prep;
+                    var target = q.First();
+                    target.PrepContent = preparation.PrepContent;
+                    target.PrepName = preparation.PrepName;
+                    campus.SaveChanges();
+                    return target;
+ 
+                }
+
+
             }
             finally
             {
--- Campus.Course/Controllers/PreparationController.cs
+++ Campus.Course/Controllers/PreparationController.cs
@@ -1,4 +1,5 @@
 ﻿using Campus.Course.Business.Interface;
+using Campus.Course.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,12 +13,14 @@ namespace Campus.Course.Controllers
         private ITeacher s_teacher = null;
         private ITimeSheet s_timesheet = null;
         private ITeach s_teach = null;
+        private IPreparation s_prep = null;
 
-        public PreparationController(ITeacher _teahcer, ITimeSheet _timesheet,ITeach _teach)
+        public PreparationController(ITeacher _teahcer, ITimeSheet _timesheet,ITeach _teach,IPreparation _prep)
         {
             s_teacher = _teahcer;
             s_timesheet = _timesheet;
             s_teach = _teach;
+            s_prep = _prep;
         }
 
         //
@@ -38,10 +41,76 @@ namespace Campus.Course.Controllers
             return View();
         }
 
-        public ActionResult GetPreparation(int prepId)
+//        {
+        //    id:
+        //    sheetid:
+        //    name:
+        //    content:
+        //    teachno:
+        //    m:[
+        //    {id:type:name:path},
+        //    {id:type:name:path}
+        //    ]
+//          }
+        public ActionResult GetPreparation(int prepId, int sheetid)
         {
-            return new EmptyResult();
+            JsonObject o = new JsonObject();
+            o.MergeProperty("sheetid", new JsonConstant(sheetid));
+            if (prepId > 0)
+            {
+                var prepInfoes = s_prep.GetPreparationByPId(null, prepId);
+
+
+                foreach (var oneprep in prepInfoes.GroupBy(m => m.PrepId))
+                {
+                    o.MergeProperty("id", new JsonConstant(oneprep.First().PrepId));                    
+                    o.MergeProperty("name", new JsonConstant(oneprep.First().PrepName ?? string.Empty));
+                    o.MergeProperty("content", new JsonConstant(oneprep.First().PrepContent ?? string.Empty));
+                    JsonCollection mcollection = new JsonCollection();
+                    o.MergeProperty("m", mcollection);
+                    foreach (var m in oneprep)
+                    {
+                        if (m.MeteiralId < 1)
+                            continue;
+                        JsonObject mo = new JsonObject();
+                        mo.MergeProperty("id", new JsonConstant(m.MeteiralId));
+                        mo.MergeProperty("type", new JsonConstant(m.MeteiralType));
+                        mo.MergeProperty("name", new JsonConstant(m.MeteiralName ?? string.Empty));
+                        mo.MergeProperty("path", new JsonConstant(m.MeteiralPath));
+                        mcollection.AppendObject(mo);
+                    }
+                }
+            }
+
+            return RawJson(o,JsonRequestBehavior.AllowGet);
  
         }
+
+        //        {
+        //    id:
+        //    sheetid:
+        //    name:
+        //    content:
+        //    teachno:
+        //    error:
+        //          }
+        [HttpPost]
+        public ActionResult SavePreparation(Preparation prep)
+        {
+            JsonObject o = new JsonObject();
+            o.MergeProperty("sheetid", new JsonConstant(prep.TeachTimeSheetId));
+            try
+            {
+                var result = s_prep.SavePreparation(null,prep);
+                o.MergeProperty("id", new JsonConstant(result.ID));
+                o.MergeProperty("name", new JsonConstant(result.PrepName ?? string.Empty));
+                o.MergeProperty("content", new JsonConstant(result.PrepContent ?? string.Empty));
+            }
+            catch(Exception ex) { 
+                o.MergeProperty("error",new JsonConstant(ex.Message));
+            }
+            return RawJson(o, JsonRequestBehavior.DenyGet);
+        }
+
     }
 }
